c80b672e6353f04682b9fd1c545f5ee59c74948f
Attila Takacs <a.takacs@nyala.de>
2022-08-12 10:32:48 +0200

    polygon - matic denom fix

--- src/app/core/services/blockchain/polygon.service.ts
+++ src/app/core/services/blockchain/polygon.service.ts
@@ -10,7 +10,7 @@ import { EthereumService } from './ethereum.service';
   providedIn: 'root'
 })
 export class PolygonService extends EthereumService {
-  nativeSymbol: string = "POLY";
+  nativeSymbol: string = "MATIC";
 
   constructor(protected config: ConfigService, protected notification: NotificationService) {
     super(config, notification);
